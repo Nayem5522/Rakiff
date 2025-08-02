e8d319149ab04469a4887e618bba5160618ec533
Nayem5522 <sahidhasannayembd@gmail.com>
2025-08-02 09:40:16 +0600

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -26,9 +26,40 @@ async def channel_post(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("de🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
 
     await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
+
+
+@Bot.on_message(filters.private & ~admin & ~filters.command(['start', 'help']))
+async def user_file_handler(client: Client, message: Message):
+    try:
+        await message.reply_photo(
+            photo="https://i.postimg.cc/9FgztwCx/IMG-20250802-093723-437.jpg",  # আপনি চাইলে নিজের পিকচার দিতে পারেন
+            caption=(
+                "🚫 **ᴘᴇʀꜱᴏɴᴀʟ ʙᴏᴛ ɴᴏᴛɪᴄᴇ**\n\n"
+                "ʏᴏᴜ ᴀʀᴇ ᴀᴛᴛᴇᴍᴘᴛɪɴɢ ᴛᴏ ᴜꜱᴇ ᴀ ʙᴏᴛ ᴛʜᴀᴛ ɪꜱ ʀᴇꜱᴇʀᴠᴇᴅ ꜰᴏʀ ᴘᴇʀꜱᴏɴᴀʟ ᴜꜱᴇ ᴏɴʟʏ.\n\n"
+                "ʙᴜᴛ ᴅᴏɴ'ᴛ ᴡᴏʀʀʏ! ᴡᴇ ʜᴀᴠᴇ ᴀ ᴘᴜʙʟɪᴄ ᴠᴇʀꜱɪᴏɴ ꜰᴏʀ ʏᴏᴜ ❤️\n\n"
+                "📦 **ᴀᴠᴀɪʟᴀʙʟᴇ ꜰᴇᴀᴛᴜʀᴇꜱ ɪɴ ᴘᴜʙʟɪᴄ ʙᴏᴛ:**\n"
+                "┏━━━━━━━━━━━━━━━┓\n"
+                "┣ 🏅 ᴘʀᴇᴍɪᴜᴍ ᴘʟᴀɴ\n"
+                "┣ 🔗 ʟɪɴᴋ ꜱʜᴏʀᴛɴᴇʀ\n"
+                "┣ ⏱️ ᴛᴏᴋᴇɴ ᴠᴇʀɪꜰɪᴄᴀᴛɪᴏɴ\n"
+                "┣ 🍿 ᴄᴜꜱᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ\n"
+                "┣ 📢 ꜰᴏʀᴄᴇ ꜱᴜʙꜱᴄʀɪʙᴇ\n"
+                "┣ 🎛️ ᴄᴜꜱᴛᴏᴍ ʙᴜᴛᴛᴏɴꜱ\n"
+                "┣ ♻️ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ\n"
+                "┣ 🔐 ᴘʀᴏᴛᴇᴄᴛ ᴄᴏɴᴛᴇɴᴛ\n"
+                "┣ 📺 ꜱᴛʀᴇᴀᴍ/ᴅᴏᴡɴʟᴏᴀᴅ\n"
+                "┗━━━━━━━━━━━━━━━┛\n\n"
+                "✅ ʏᴏᴜ ᴄᴀɴ ᴄᴜꜱᴛᴏᴍɪᴢᴇ ᴇᴠᴇʀʏᴛʜɪɴɢ ꜰʀᴏᴍ ᴀ ᴛᴏ ᴢ, ꜱᴏ ᴅᴏɴ'ᴛ ᴅᴇʟᴀʏ, ᴛʀʏ ɪᴛ ɴᴏᴡ.👇"
+            ),
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("🔗 ᴏᴘᴇɴ ᴘᴜʙʟɪᴄ ʙᴏᴛ", url="https://t.me/File_Store_Prime_Bot")]
+            ])
+        )
+    except Exception as e:
+        print(e)
